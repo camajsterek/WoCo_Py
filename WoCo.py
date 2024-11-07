71c184c7ccdb30d33e4bbeb2d62b4dd9af038fac
camajsterek <camajsterek+github@gmail.com>
2024-11-07 10:16:41 +0000

    update

---
+++ WoCo.py
@@ -0,0 +1,2 @@
+# A program that finds the most frequently used words within a document and the context for them.
+# This is a project requested by my wife which will hopefuly expand beyond its initial scope.
